9f6e4f02989dbc4af1de28d681e094d94720b9b2
Harlan Kellaway <harlan.github@gmail.com>
2021-02-28 14:40:30 -0500

    Copy to ep05 starter from ep04 final

---
+++ 05-adapt-to-dynamic-type/starter/ContrastPicker/ContrastPicker/Supporting Files/RGB.swift
@@ -0,0 +1,54 @@
+/// Copyright (c) 2021 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+public class RGB: ObservableObject {
+  let r: Double
+  let g: Double
+  let b: Double
+
+  var luminance: Double {
+    0.2126 * lum(r) + 0.7152 * lum(g) + 0.0722 * lum(b)
+  }
+
+  private func lum(_ value: Double) -> Double {
+    value <= 0.03928 ? value/12.92
+      : pow((value + 0.055)/1.055, 2.4)
+  }
+
+  init(r: Double, g: Double, b: Double) {
+    self.r = r
+    self.g = g
+    self.b = b
+  }
+}
